282919baf6fc361e161505fbb2beb57e48b5f70f
basilkottilingal <basilkottilingal@gmail.com>
2025-02-05 06:12:17 +0100

    Client class added. NOTE: server not yet ready TODO: class hierarchy TOP: Chess, Client, LOWER: ChessGame, LOWEST: EventListen

--- chessjs/client.js
+++ chessjs/client.js
@@ -1,5 +1,3 @@
-// error log for user, that will be displayed on the page
-const errorLog = document.getElementById("errorLog");
 
 const ENCODE = {
   handshake: 0x0, // send/recv
@@ -17,152 +15,148 @@ const ENCODE = {
   debug:   0xf  // send/recv
 };
 
-function encode(msgtype, binaryMsg) {
-  let combined =  
-    new Uint8Array(1 + binaryMsg.length);
-  combined[0] = msgtype;
-  combined.set(binaryMsg, 1);
-  return combined.buffer;
-}
-
-function encodeText(info, msg) {
-  let combined =  
-    new Uint8Array(2 + msg.length);
-  combined[0] = ENCODE.text;
-  combined[1] = info;
-  let encoder = new TextEncoder();
-  combined.set(encoder.encode(msg), 2);
-  console.log("Sending:: type:",info," msg:", msg )
-  return combined.buffer;
-}
-
-function decode(msg) {
-  let type = msg[0];
-  let start = (type === ENCODE.text) ? 2 : 1;
-
-  //let decoder = new TextDecoder();
-  //let text = decoder.decode(msg.subarray(start));
-  console.log("Decode received:", text, "len", msg.length, " start", start, " type", type);
-}
-
-
-// This file should be written by wsserver.c !!!
-const socket = 
-  new WebSocket("ws://localhost:8080");
-// Ensures data is received as ArrayBuffer 
-socket.binaryType = "arraybuffer"; 
-
+class Client {
   
-socket.onopen = function () {
-  console.log("Connected to wsServer");
-};
+  constructor(){
 
-socket.onmessage = function (event) {
-  //console.log("Received:", event.data);
-  if (event.data instanceof ArrayBuffer) {
-    let uint8Array = new Uint8Array(event.data);
-    console.log(uint8Array);
-    // console.log("Received Binary Data:", uint8Array);
-    // Example: Convert binary to string 
-    decode(event.data);
-  } else if (event.data instanceof Blob) {
-    console.log("ERROR : Weird!! socket binary type is arraybuffer");
+    //Error 
+    this.errorLog = document.getElementById("errorLog");
+    // FEN Input 
+    this.inputFEN = document.getElementById("fen");
+    this.submitFEN = document.getElementById("fenEnter");
+
+    // This file should be written by wsserver.c !!!
+    this.socket = 
+      new WebSocket("ws://localhost:8080");
+    // Ensures data is received as ArrayBuffer 
+    this.socket.binaryType = "arraybuffer"; 
+
+    this.socket.onopen = function () {
+      console.log("Connected to wsServer");
+    };
+
+    this.socket.onmessage = function (event) {
+      //console.log("Received:", event.data);
+      if (event.data instanceof ArrayBuffer) {
+        let uint8Array = new Uint8Array(event.data);
+        console.log(uint8Array);
+        // console.log("Received Binary Data:", uint8Array);
+        // Example: Convert binary to string 
+        this.decode(event.data);
+      } else if (event.data instanceof Blob) {
+          console.log("ERROR : Weird!! socket binary type is arraybuffer");
+      }
+      else {
+        console.log("Received Non-Binary Data:", event.data);
+      }
+    };
+
+    this.socket.onclose = function () {
+      console.log("Connection Lost");
+      this.errorLog.textContent = "Server Error: Connection Lost!";
+    };
+  } // End of the default contructor
+
+  //function 
+  encode(msgtype, binaryMsg) {
+    let combined =  
+      new Uint8Array(1 + binaryMsg.length);
+    combined[0] = msgtype;
+    combined.set(binaryMsg, 1);
+    return combined.buffer;
   }
-  else {
-    console.log("Received Non-Binary Data:", event.data);
+
+  //function 
+  encodeText(info, msg) {
+    let combined =  
+      new Uint8Array(2 + msg.length);
+    combined[0] = ENCODE.text;
+    combined[1] = info;
+    let encoder = new TextEncoder();
+    combined.set(encoder.encode(msg), 2);
+    console.log("Sending:: type:",info," msg:", msg )
+    return combined.buffer;
   }
-};
 
-socket.onclose = function () {
-  console.log("Connection Lost");
-  errorLog.textContent = "Server Error: Connection Lost!";
-};
-
+  //function 
+  decode(msg) {
+    let type = msg[0];
+    let start = (type === ENCODE.text) ? 2 : 1;
+  }
     
-// Wait for the DOM to load before executing the function
-document.addEventListener('DOMContentLoaded', function() {
+  // Send input FEN to server
+  //function 
+  sendFen() {
+    let fen = this.inputFEN.value.trim();
+    if( fen.length > 22 ) {
+      this.socket.send(fen);
+      //if invalid
+      this.inputFEN.value = "";
+      this.inputFEN.disabled = true;
+      this.submitFEN.disabled = true;
+      this.errorLog.textContent = "";
+    }
+    else {
+      this.errorLog.textContent = "invalid fen! Please enter a valid one";
+      console.log("Invalid FEN");
+    }
+  }
+
+  //List all eventListen of buttons, etc
+  //function 
+  eventListen(){
+ 
+    // Restart button 
+    document.addEventListener('DOMContentLoaded', function() {
       // Get the button element by ID
-  const button = document.getElementById('restart');
+      const button = document.getElementById('restart');
       // Add the event listener for the click event
-  button.addEventListener('click', function() {
-    //alert('Button clicked!');
-    socket.send("Restart");
-  });
-});
+      button.addEventListener('click', function() {
+        //alert('Button clicked!');
+        this.socket.send("Restart");
+      });
+    });
    
-/* 
-document.addEventListener('DOMContentLoaded', function() {
+    // Undo button
+    document.addEventListener('DOMContentLoaded', function() {
       // Get the button element by ID
-  const button = document.getElementById('handshake');
+      const button = document.getElementById('undo');
       // Add the event listener for the click event
-  button.addEventListener('click', function() {
-    //alert('Button clicked!');
-    socket.send("Hello Server!");
-  });
-});
-*/
+      button.addEventListener('click', function() {
+        //alert('Button clicked!');
+        //socket.send("undo");
+        console.log("Sending:: type:",ENCODE.undo," msg:", "undo" )
+        socket.send(encodeText(ENCODE.undo, "undo"));
+      });
+    });
 
-document.addEventListener('DOMContentLoaded', function() {
-      // Get the button element by ID
-  const button = document.getElementById('undo');
-      // Add the event listener for the click event
-  button.addEventListener('click', function() {
-    //alert('Button clicked!');
-    //socket.send("undo");
-    console.log("Sending:: type:",ENCODE.undo," msg:", "undo" )
-    socket.send(encodeText(ENCODE.undo, "undo"));
-  });
-});
+    let debounceTimer;
 
-  
-/*
-// Add an event listener for the 'input' event
-document.getElementById("fen").addEventListener("blur", function(event) {
-  // Update the output element with the value of the input field
-  document.getElementById("fenValidity").textContent = "FEN: " + event.target.value;
+    // Handle the input event with debounce
+    this.inputFEN.addEventListener("input", function() {
+      clearTimeout(debounceTimer); // Clear the previous timer
+      debounceTimer = setTimeout(function() {
+        //
+      }, 500); // Adjust 500ms as needed
+    });
 
-});
-*/
+    // Trigger on Enter key press
+    this.inputFEN.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        //event.preventDefault(); // Prevent form submission
+        this.sendFen();
+      }
+    });
 
-const inputFEN = document.getElementById("fen");
-const submitFEN = document.getElementById("fenEnter");
+    // Trigger on button click
+    this.submitFEN.addEventListener("click", function() {
+      this.sendFen();
+    });
+  } // End of eventListen()
+} // End of the class "Client"
 
 
-let debounceTimer;
-
-// Function to handle the result
-function sendFen() {
-  let fen = inputFEN.value.trim();
-  if( fen.length > 22 ) {
-    socket.send(fen);
-    //if invalid
-    inputFEN.value = "";
-    inputFEN.disabled = true;
-    submitFEN.disabled = true;
-    errorLog.textContent = "";
-  }
-  else {
-    errorLog.textContent = "invalid fen! Please enter a valid one";
-    console.log("Invalid FEN");
-  }
-}
-
-// Handle the input event with debounce
-inputFEN.addEventListener("input", function() {
-  clearTimeout(debounceTimer); // Clear the previous timer
-  debounceTimer = setTimeout(function() {
-  }, 500); // Adjust 500ms as needed
-});
-
-// Trigger on Enter key press
-inputFEN.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    //event.preventDefault(); // Prevent form submission
-    sendFen();
-  }
-});
-
-// Trigger on button click
-submitFEN.addEventListener("click", function() {
-  sendFen();
-});
+/* COnstructor opens socket*/
+const socket = new Client();
+/* Enable all the buttons input field, etc */
+socket.eventListen();
